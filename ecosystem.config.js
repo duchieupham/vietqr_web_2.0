c2dad98bdd880f3a4cd3d05a346c025a90538c8b
Smjle <kien.nh@vietqr.vn>
2024-08-26 21:46:27 +0700

    change ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,22 @@
 module.exports = {
   apps: [
     {
+      name: 'vietqr_web',
       script: 'npm start',
+      env_production: {
+        NODE_ENV: 'production',
+      },
     },
   ],
 
   deploy: {
     production: {
-      ket: 'key.pem',
+      key: 'key.pem',
       user: 'root',
       host: '171.244.63.153',
       ref: 'origin/product',
       repo: 'https://github.com/duchieupham/vietqr_web_2.0.git',
-      path: '/home/ubuntu',
+      path: '/var/www/vietqr_web_2.0',
       'pre-deploy-local': '',
       'post-deploy':
         'source ~/.nvm/nvm.sh && npm install && pm2 reload ecosystem.config.js --env production',
