dadffbc78abd54c296caa37f7c2552c1e8a2dd99
minhnhat-vietqr <minh.pn@vietqr.vn>
2024-09-01 12:16:54 +0700

    Refactor login page layout for mobile and tablet devices

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -19,72 +19,68 @@ function Login() {
   }, []);
 
   return (
-    <Box height="100vh">
+    <Box
+      sx={{
+        minHeight: '100vh',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      }}
+    >
+      <Box component="nav">
+        <Navbar />
+      </Box>
       <Stack
-        // height="100%"
+        component="main"
         sx={{
-          flexDirection: 'column',
-          justifyContent: 'space-between',
+          p: {
+            xxs: '0 16px',
+            xs: '0 24px',
+            sm: '0 32px',
+            md: '0 40px',
+          },
+          gap: {
+            xxs: 2,
+            xs: 4,
+            lg: 6,
+          },
+          flexDirection: {
+            xxs: 'column-reverse',
+            md: 'row',
+          },
+          justifyContent: {
+            xxs: 'center',
+            md: 'flex-end',
+            lg: 'center',
+          },
+          height: '100%',
+          alignItems: 'center',
+          flexGrow: 1,
+          minHeight: {
+            xxs: '60vh',
+            md: '66vh',
+            lg: '68vh',
+            // xl: '74vh',
+            // xxl: '78vh',
+          },
+          marginTop: {
+            xxs: 2,
+            sm: 4,
+          },
         }}
       >
-        <Box component="nav">
-          <Navbar />
-        </Box>
-        <Stack
-          component="main"
-          sx={{
-            p: {
-              xxs: '0 16px',
-              xs: '0 24px',
-              sm: '0 32px',
-              md: '0 40px',
-            },
-            gap: {
-              xxs: 2,
-              xs: 4,
-              lg: 6,
-            },
-            flexDirection: {
-              xxs: 'column-reverse',
-              md: 'row',
-            },
-            justifyContent: {
-              xxs: 'center',
-              md: 'flex-end',
-              lg: 'center',
-            },
-            height: '100%',
-            alignItems: 'center',
-            minHeight: {
-              xxs: '60vh',
-              md: '66vh',
-              // lg: '68vh',
-              // xl: '74vh',
-              // xxl: '78vh',
-            },
-          }}
-        >
-          <CreateQR />
-          <LoginForm />
-        </Stack>
-        <Box
-          component="footer"
-          sx={{
-            mt: {
-              xxs: 'auto',
-            },
-            width: '100%',
-            position: 'absolute',
-            bottom: 0,
-            top: {
-              xxs: '70%',
-              lg: '75%',
-            },
-          }}
-        >
-          <FooterLogin />
-        </Box>
+        <CreateQR />
+        <LoginForm />
       </Stack>
+      <Box
+        component="footer"
+        sx={{
+          mt: 'auto',
+          width: '100%',
+          position: 'absolute',
+        }}
+      >
+        <FooterLogin />
+      </Box>
     </Box>
   );
 }
