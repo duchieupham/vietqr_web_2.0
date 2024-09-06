6c52e029a4f798ca2821f26fbbbd9630dfd0cc40
minhnhat-vietqr <minh.pn@vietqr.vn>
2024-09-06 14:17:35 +0700

    refactor: remove unused IconButtonAnimate component and AnimateWrap function

--- src/components/IconButtonAnimate.js
+++
@@ -1,9 +0,0 @@
-import { Box } from '@mui/material';
-
-export function IconButtonAnimate() {
-  return <div>IconButtonAnimate</div>;
-}
-
-export function AnimateWrap({ size, children }) {
-  return <Box>{children}</Box>;
-}
