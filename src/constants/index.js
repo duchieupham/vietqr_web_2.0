3ae9c68ff627120b2cfcf6615948c49cdac8c7cc
minhnhat-vietqr <minh.pn@vietqr.vn>
2024-09-11 09:21:11 +0700

    refactor: Update constants/index.js with new dashboard paths

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,5 @@
+import { set } from 'lodash-es';
+
 // cookie
 export const LOCALE_COOKIE = 'LOCALE';
 export const AUTH_COOKIE = 'AUTH_TOKEN';
@@ -14,10 +16,62 @@ export const PATH_PAGE = {
 };
 export const DEFAULT_PATH = '/dashboard';
 export const PUBLIC_PATHS = ['/', '/login', '/register', '/notice'];
+export const HOME_DASHBOARD_PATHS = {
+  general: {
+    fastFeatures: '/general/fast-features',
+    VQRCommunity: {
+      qrNewFeeds: '/general/VQR-community/qr-new-feeds',
+      news: '/general/VQR-community/news',
+      crossSellingItems: '/general/VQR-community/cross-selling-items',
+    },
+  },
+  transaction: '/transaction',
+  bankAccount: '/bank-account',
+  extensions: '/extensions',
+  setting: '/setting',
+};
+export const AGENT_DASHBOARD_PATHS = {
+  businessInformation: {
+    agentInformation: '/business-information/agent-information',
+    servicesConnect: '/business-information/services-connect',
+    promotionPrograms: '/business-information/promotion-programs',
+  },
+  sales: {
+    managementSales: '/sales/sales-management',
+    managementRefunds: '/sales/refund-management',
+    createOrder: '/sales/create-order',
+  },
+  statisticalReport: {
+    salesStatistics: '/report-statistics/sales-statistics',
+    commodityStatistics: '/report-statistics/commodity-statistics',
+    customerStatistics: '/report-statistics/customer-statistics',
+  },
+  storeAndPointOfSale: {
+    storeList: '/store-pos/store-list',
+    addNewStore: '/store-pos/add-new-store',
+  },
+  employees: {
+    employeeList: '/employees/employee-list',
+    addNewEmployee: '/employees/add-new-employee',
+  },
+  customers: {
+    customerList: '/customers/customer-list',
+    addNewCustomer: '/customers/add-new-customer',
+  },
+  products: {
+    productList: '/products/product-list',
+    addNewProduct: '/products/add-new-product',
+    inventoryManagement: '/products/inventory-management',
+  },
+  support: {
+    helpCenter: '/support/help-center',
+  },
+};
 
 // regex
 export const PHONE_REGEX =
   /(?:\+84|0084|0)[235789][0-9]{1,2}[0-9]{7}(?:[^\d]+|$)/g;
+export const NUMBER_REGEX = /^[0-9]*$/;
 export const PHONE_LENGTH_REGEX = /^\d{10}$/;
 export const PASSWORD_LENGTH_REGEX = /^\d{6}$/;
 
