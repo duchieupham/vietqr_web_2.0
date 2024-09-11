2910501c613f2b00c2cbe8351fe4e4e58447cf06
minhnhat-vietqr <minh.pn@vietqr.vn>
2024-09-11 09:37:34 +0700

    refactor: Update sidebar menu content structure and styling

--- src/constants/dashboard.js
+++ src/constants/dashboard.js
@@ -7,13 +7,45 @@ export const DASHBOARD_TYPE = [
     routes: [
       {
         id: 'general',
-        label: 'general',
+        label: 'General',
         path: '/',
+        subRoutes: [
+          {
+            id: 'qrNewFeeds',
+            label: 'QR New Feeds',
+            path: '/general/qr-new-feeds',
+          },
+          {
+            id: 'news',
+            label: 'News',
+            path: '/general/news',
+          },
+          {
+            id: 'crossSellingItems',
+            label: 'Cross-Selling Items',
+            path: '/general/cross-selling-items',
+          },
+        ],
       },
       {
         id: 'transaction',
-        label: 'transaction management',
+        label: 'Transaction Management',
         path: '/transaction',
+      },
+      {
+        id: 'bankAccount',
+        label: 'Bank Account',
+        path: '/bank-account',
+      },
+      {
+        id: 'extensions',
+        label: 'Extensions',
+        path: '/extensions',
+      },
+      {
+        id: 'setting',
+        label: 'Setting',
+        path: '/setting',
       },
     ],
   },
@@ -23,14 +55,148 @@ export const DASHBOARD_TYPE = [
     path: '/dashboard/agent',
     routes: [
       {
-        id: 'business',
-        label: 'business information',
-        path: '/business',
+        id: 'businessInformation',
+        label: 'Business Information',
+        path: '/businessInformation',
+        subRoutes: [
+          {
+            id: 'agentInformation',
+            label: 'Agent Information',
+            path: '/business-information/agent-information',
+          },
+          {
+            id: 'servicesConnect',
+            label: 'Services Connect',
+            path: '/business-information/services-connect',
+          },
+          {
+            id: 'promotionPrograms',
+            label: 'Promotion Programs',
+            path: '/business-information/promotion-programs',
+          },
+        ],
       },
       {
-        id: 'sale',
-        label: 'sale management',
-        path: '/sale',
+        id: 'sales',
+        label: 'Sales Management',
+        path: '/sales',
+        subRoutes: [
+          {
+            id: 'managementSales',
+            label: 'Sales Management',
+            path: '/sales/sales-management',
+          },
+          {
+            id: 'managementRefunds',
+            label: 'Refund Management',
+            path: '/sales/refund-management',
+          },
+          {
+            id: 'createOrder',
+            label: 'Create Order',
+            path: '/sales/create-order',
+          },
+        ],
+      },
+      {
+        id: 'statisticalReport',
+        label: 'Statistical Report',
+        path: '/statistical-report',
+        subRoutes: [
+          {
+            id: 'salesStatistics',
+            label: 'Sales Statistics',
+            path: '/report-statistics/sales-statistics',
+          },
+          {
+            id: 'commodityStatistics',
+            label: 'Commodity Statistics',
+            path: '/report-statistics/commodity-statistics',
+          },
+          {
+            id: 'customerStatistics',
+            label: 'Customer Statistics',
+            path: '/report-statistics/customer-statistics',
+          },
+        ],
+      },
+      {
+        id: 'storeAndPointOfSale',
+        label: 'Store & Point of Sale',
+        path: '/store-pos',
+        subRoutes: [
+          {
+            id: 'storeList',
+            label: 'Store List',
+            path: '/store-pos/store-list',
+          },
+          {
+            id: 'addNewStore',
+            label: 'Add New Store',
+            path: '/store-pos/add-new-store',
+          },
+        ],
+      },
+      {
+        id: 'employees',
+        label: 'Employees',
+        path: '/employees',
+        subRoutes: [
+          {
+            id: 'employeeList',
+            label: 'Employee List',
+            path: '/employees/employee-list',
+          },
+          {
+            id: 'addNewEmployee',
+            label: 'Add New Employee',
+            path: '/employees/add-new-employee',
+          },
+        ],
+      },
+      {
+        id: 'customers',
+        label: 'Customers',
+        path: '/customers',
+        subRoutes: [
+          {
+            id: 'customerList',
+            label: 'Customer List',
+            path: '/customers/customer-list',
+          },
+          {
+            id: 'addNewCustomer',
+            label: 'Add New Customer',
+            path: '/customers/add-new-customer',
+          },
+        ],
+      },
+      {
+        id: 'products',
+        label: 'Products',
+        path: '/products',
+        subRoutes: [
+          {
+            id: 'productList',
+            label: 'Product List',
+            path: '/products/product-list',
+          },
+          {
+            id: 'addNewProduct',
+            label: 'Add New Product',
+            path: '/products/add-new-product',
+          },
+          {
+            id: 'inventoryManagement',
+            label: 'Inventory Management',
+            path: '/products/inventory-management',
+          },
+        ],
+      },
+      {
+        id: 'support',
+        label: 'Support',
+        path: '/support',
       },
     ],
   },
--- src/constants/index.js
+++ src/constants/index.js
@@ -16,57 +16,6 @@ export const PATH_PAGE = {
 };
 export const DEFAULT_PATH = '/dashboard';
 export const PUBLIC_PATHS = ['/', '/login', '/register', '/notice'];
-export const HOME_DASHBOARD_PATHS = {
-  general: {
-    fastFeatures: '/general/fast-features',
-    VQRCommunity: {
-      qrNewFeeds: '/general/VQR-community/qr-new-feeds',
-      news: '/general/VQR-community/news',
-      crossSellingItems: '/general/VQR-community/cross-selling-items',
-    },
-  },
-  transaction: '/transaction',
-  bankAccount: '/bank-account',
-  extensions: '/extensions',
-  setting: '/setting',
-};
-export const AGENT_DASHBOARD_PATHS = {
-  businessInformation: {
-    agentInformation: '/business-information/agent-information',
-    servicesConnect: '/business-information/services-connect',
-    promotionPrograms: '/business-information/promotion-programs',
-  },
-  sales: {
-    managementSales: '/sales/sales-management',
-    managementRefunds: '/sales/refund-management',
-    createOrder: '/sales/create-order',
-  },
-  statisticalReport: {
-    salesStatistics: '/report-statistics/sales-statistics',
-    commodityStatistics: '/report-statistics/commodity-statistics',
-    customerStatistics: '/report-statistics/customer-statistics',
-  },
-  storeAndPointOfSale: {
-    storeList: '/store-pos/store-list',
-    addNewStore: '/store-pos/add-new-store',
-  },
-  employees: {
-    employeeList: '/employees/employee-list',
-    addNewEmployee: '/employees/add-new-employee',
-  },
-  customers: {
-    customerList: '/customers/customer-list',
-    addNewCustomer: '/customers/add-new-customer',
-  },
-  products: {
-    productList: '/products/product-list',
-    addNewProduct: '/products/add-new-product',
-    inventoryManagement: '/products/inventory-management',
-  },
-  support: {
-    helpCenter: '/support/help-center',
-  },
-};
 
 // regex
 export const PHONE_REGEX =
