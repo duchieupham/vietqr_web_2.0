ee06d61e8edcdef62cadfed7f7faa7d7b1271c3f
minhnhat-vietqr <minh.pn@vietqr.vn>
2024-09-13 16:28:46 +0700

    remove: the space in name file

---
+++ src/components/footer/login/FooterLogin.js
@@ -0,0 +1,90 @@
+import { Box, Grid } from '@mui/material';
+import { useEffect, useState } from 'react';
+import imagesAPI from '~/api/images/imagesService';
+import { CarouselSlider, Contact, Social } from '~/sections/login/footer';
+
+function Footer() {
+  const [bankType, setBankType] = useState([]);
+
+  const bankTypeList = async () => {
+    try {
+      const res = await imagesAPI.bankTypeList();
+      // console.log('API Response:', res.data); // Debugging
+      setBankType(res.data);
+    } catch (error) {
+      // console.error('Error fetching bank types:', error);
+    }
+  };
+
+  useEffect(() => {
+    bankTypeList();
+  }, []);
+
+  return (
+    <Box
+      sx={{
+        px: {
+          xs: 0,
+        },
+        pt: {
+          xxl: '1.5rem',
+        },
+      }}
+    >
+      <Grid
+        container
+        item
+        columns={12}
+        sx={{
+          flexDirection: {
+            xs: 'column',
+            md: 'row',
+          },
+          alignItems: {
+            xs: 'center',
+            lg: 'flex-start',
+          },
+        }}
+      >
+        <Grid
+          item
+          xs={12}
+          md={6}
+          sx={{
+            width: '100%',
+            position: 'relative',
+          }}
+        >
+          <Contact
+            style={{
+              flexWrap: {
+                xs: 'wrap',
+                md: 'nowrap',
+              },
+            }}
+            stackStyle={{
+              margin: {
+                xs: '0 1rem',
+                sm: '0 3rem',
+              },
+              whiteSpace: 'nowrap',
+              flexGrow: 1,
+              marginBottom: {
+                xs: '0.5rem',
+                sm: '1rem',
+                md: '0.5rem',
+              },
+            }}
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Social />
+        </Grid>
+      </Grid>
+      <Box>
+        <CarouselSlider initialValues={bankType} />
+      </Box>
+    </Box>
+  );
+}
+export default Footer;
