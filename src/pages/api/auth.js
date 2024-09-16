1306bd71263ab7579c243546927a779a3982d269
minhdt-vietqr <minh.dt@vietqr.vn>
2024-09-16 11:05:16 +0700

    fix: improve auth api

--- src/pages/api/auth.js
+++ src/pages/api/auth.js
@@ -2,16 +2,20 @@ import { setCookie } from 'cookies-next';
 import { AUTH_COOKIE } from '~/constants';
 
 export default function handler(req, res) {
-  const { token, exp } = req.body;
-  const expire = new Date(Date.now() + 1 * 24 * 60 * 60 * 1000); // 1 days
+  try {
+    const { token, exp } = req.body;
+    const expire = new Date(Date.now() + 1 * 24 * 60 * 60 * 1000); // 1 days
 
-  setCookie(AUTH_COOKIE, token, {
-    req,
-    res,
-    secure: true,
-    expire: exp || expire,
-    sameSite: 'strict',
-  });
+    setCookie(AUTH_COOKIE, token, {
+      req,
+      res,
+      secure: true,
+      expire: exp || expire,
+      sameSite: 'strict',
+    });
 
-  res.status(200).json({ message: 'success' });
+    res.status(200).json({ status: 'success' });
+  } catch (e) {
+    res.status(400).json({ status: 'error', message: e.toString() });
+  }
 }
