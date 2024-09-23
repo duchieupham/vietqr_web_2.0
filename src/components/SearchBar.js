f78bf26d2e239606e83075b6533be0e9466f0513
minhnhat-vietqr <minh.pn@vietqr.vn>
2024-09-23 22:08:12 +0700

    refactor

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,4 @@
-import { Box, Button, Typography } from '@mui/material';
-import { useTranslations } from 'next-intl';
-import Image from 'next/image';
-import { useAuthContext } from '~/contexts/hooks';
+import { Box } from '@mui/material';
 import ExpandSearchBar from '~/sections/dashboard/searchbar/ExpandSearchBar';
 
 export default function SearchBar() {
@@ -9,55 +6,5 @@ export default function SearchBar() {
     <Box sx={{ display: 'flex', alignItems: 'center', position: 'relative' }}>
       <ExpandSearchBar />
     </Box>
-  );
-}
-function ExpandSearchBoxButton() {
-  const { session } = useAuthContext();
-  const t = useTranslations();
-  return (
-    <Button
-      sx={{
-        borderRadius: '8px',
-        display: 'flex',
-        background: '#F0F4FA',
-        cursor: 'pointer',
-        width: 250,
-      }}
-      disableRipple
-    >
-      {/* Icon */}
-      <Box
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-        }}
-      >
-        <Image
-          src="/icons/search-icon-solid.svg"
-          width={30}
-          height={30}
-          alt="search-icon"
-        />
-      </Box>
-      {/* TextField  */}
-      <Box
-        sx={{
-          '& .MuiTypography-root': {
-            justifyContent: 'center',
-            alignItems: 'center',
-            color: '#666A72',
-            fontStyle: 'italic',
-            fontSize: {
-              xs: '8px',
-              sm: '12px',
-            },
-          },
-        }}
-      >
-        <Typography>
-          {`${t('Hello')} ${session?.firstName}, ${t('search')}`}
-        </Typography>
-      </Box>
-    </Button>
   );
 }
