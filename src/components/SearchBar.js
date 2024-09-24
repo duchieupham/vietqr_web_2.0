4d978bdb1b1d4ea56480aed38a97195cf4216b0e
minhnhat-vietqr <minh.pn@vietqr.vn>
2024-09-24 15:48:07 +0700

    Refactor SearchBar component and add search functionality

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,25 +1,38 @@
 import { Box, styled } from '@mui/material';
-import { useAppSelector } from '~/redux/hook';
+import { useState } from 'react';
 import { CollapseSearchBar } from '~/sections/dashboard/searchbar';
 import ExpandSearchBar from '~/sections/dashboard/searchbar/ExpandSearchBar';
 
 export default function SearchBar() {
-  const { isExpanded } = useAppSelector((state) => state.search);
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [contexts, setContexts] = useState([]);
+  const [isSearch, setIsSearch] = useState(false);
+  const [isNoContext, setIsNoContext] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const isEmpty = !contexts || contexts.length === 0;
+
+  const onClickExpandedSearchBar = () => {
+    setIsExpanded((prev) => !prev);
+  };
+
   return (
-    <SearchBarContainer>
+    // SearchBarContainer
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        position: 'relative',
+      }}
+    >
       {isExpanded ? (
         // Expanded searchbar
-        <ExpandSearchBar />
+        <ExpandSearchBar isExpanded onClick={onClickExpandedSearchBar} />
       ) : (
         // Default searchbar
         <CollapseSearchBar />
       )}
-    </SearchBarContainer>
+    </Box>
   );
 }
-
-const SearchBarContainer = styled(Box)(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  position: 'relative',
-}));
