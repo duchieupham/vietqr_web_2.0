f5aedac7fd01623944542f9796cd8259008eeda7
minhnhat-vietqr <minh.pn@vietqr.vn>
2024-09-30 21:25:10 +0700

    Refactor DashboardHeader component to update spacing values

--- src/sections/dashboard/sidebar/index.js
+++ src/sections/dashboard/sidebar/index.js
@@ -1,3 +1,2 @@
 export { default as HorizontalSidebar } from './HorizontalSidebar';
-export { default as MenuContent } from './MenuContent';
 export { default as VerticalSidebar } from './VerticalSidebar';
