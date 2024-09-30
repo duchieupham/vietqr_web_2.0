d2a2a3fffd935c03fac457fde2dde5449aea49ef
minhnhat-vietqr <minh.pn@vietqr.vn>
2024-09-30 12:53:40 +0700

    Refactor VerticalSidebar component

--- src/sections/dashboard/sidebar/VerticalSidebar.js
+++ src/sections/dashboard/sidebar/VerticalSidebar.js
@@ -1,3 +1,4 @@
+import { useTheme } from '@emotion/react';
 import {
   Box,
   Drawer,
@@ -7,14 +8,16 @@ import {
   Stack,
   styled,
   Typography,
+  useMediaQuery,
 } from '@mui/material';
 import { useTranslations } from 'next-intl';
 import Image from 'next/image';
 import { usePathname, useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { DASHBOARD_TYPE } from '~/constants/dashboard';
 import DashboardMode from '~/layout/dashboard/header/DashboardMode';
-import { useAppSelector } from '~/redux/hook';
+import { useAppDispatch, useAppSelector } from '~/redux/hook';
+import { setDashboardType } from '~/redux/slices/appSlice';
 
 const DRAWER_WIDTH = 250;
 const DRAWER_WIDTH_COLLAPSED = 70;
@@ -27,95 +30,263 @@ export default function VerticalSidebar() {
   const router = useRouter();
   const pathname = usePathname();
   const t = useTranslations();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+  const dispatch = useAppDispatch();
 
   const [isOpen, setIsOpen] = useState(true);
-  const [anchorEl, setAnchorEl] = useState(null);
+
+  const otherItems = displayedType.children.filter(
+    (item) => t(item.label) !== 'Cài đặt',
+  );
+  const settingsItem = displayedType.children.find(
+    (item) => t(item.label) === 'Cài đặt',
+  );
+
+  useEffect(() => {
+    const foundType = DASHBOARD_TYPE.find((type) =>
+      pathname.includes(type.path),
+    );
+    if (foundType) {
+      dispatch(setDashboardType(foundType?.id));
+    }
+  }, [pathname]);
 
   return (
-    <DrawerStyled variant="permanent" anchor="left" open={isOpen}>
-      <StackStyled>
-        <Box
-          sx={{
-            height: '50px',
-            display: 'flex',
-            alignItems: 'center',
-            marginBottom: '40px',
-            paddingX: '16px',
-            paddingTop: '16px',
-            justifyContent: 'space-between',
-          }}
-        >
-          <Image
-            src="/images/Logo_min.png"
-            width={58}
-            height={24}
-            priority
-            quality={100}
-            alt="VietQR Logo"
-          />
-          <DashboardMode mode="vertical" />
-        </Box>
-        <List dense disablePadding>
-          {displayedType.children.map((item) => (
-            <ListItem key={item.id} disablePadding>
-              <ListItemButtonStyled selected={pathname.includes(item.path)}>
-                {/* <ListItemIcon>{item.icon}</ListItemIcon> */}
-                <Typography
+    <Box sx={{ position: 'relative' }}>
+      <DrawerStyled variant="permanent" anchor="left" open={isOpen}>
+        <StackStyled>
+          <Box
+            sx={{
+              height: '50px',
+              display: 'flex',
+              alignItems: 'center',
+              marginBottom: '40px',
+              paddingX: '16px',
+              paddingTop: '16px',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Image
+              src={
+                isOpen
+                  ? '/images/Logo_min.png'
+                  : '/images/ic-viet-qr-small-trans.svg'
+              }
+              width={58}
+              height={24}
+              priority
+              quality={100}
+              alt="VietQR Logo"
+              onClick={() => router.push('/')}
+            />
+            {isOpen && <DashboardMode mode="vertical" />}
+          </Box>
+          <Box sx={{ flexGrow: 1 }}>
+            <List dense disablePadding>
+              {otherItems.map((item) => (
+                <ListItem
+                  key={item.id}
+                  disablePadding
+                  sx={{ paddingBottom: '8px' }}
+                >
+                  {item.children.length > 0 ? (
+                    <Box>
+                      {/* Parent Item */}
+                      {isOpen && (
+                        <Typography
+                          sx={{
+                            fontSize: {
+                              xs: '10px',
+                              md: '12px',
+                            },
+                            color: '#666A72',
+                            alignItems: 'end',
+                            paddingLeft: '16px',
+                          }}
+                        >
+                          {t(item.label)}
+                        </Typography>
+                      )}
+                      {/* Nested Children List */}
+                      <List disablePadding>
+                        {item.children.map((child) => (
+                          <ListItem key={child.id} disablePadding>
+                            <ListItemButtonStyled
+                              selected={pathname.includes(child.path)}
+                              disableRipple
+                              sx={{
+                                paddingLeft: '18px',
+                              }}
+                              onClick={() => router.push(child.path)}
+                            >
+                              <Box sx={{ display: 'flex' }}>
+                                <Image
+                                  src={child.icon || '/icons/star-gradient.svg'}
+                                  width={30}
+                                  height={30}
+                                  alt="icon"
+                                />
+                                {isOpen && (
+                                  <Typography
+                                    sx={{
+                                      fontSize: {
+                                        xs: '10px',
+                                        md: '12px',
+                                      },
+                                    }}
+                                  >
+                                    {t(child.label)}
+                                  </Typography>
+                                )}
+                              </Box>
+                            </ListItemButtonStyled>
+                          </ListItem>
+                        ))}
+                      </List>
+                    </Box>
+                  ) : (
+                    <ListItemButtonStyled
+                      selected={pathname.includes(item.path)}
+                      disableRipple
+                      onClick={() => router.push(item.path)}
+                    >
+                      {isOpen ? (
+                        <Typography
+                          sx={{
+                            fontSize: {
+                              xs: '10px',
+                              md: '12px',
+                            },
+                            color: '#666A72',
+                          }}
+                        >
+                          {t(item.label)}
+                        </Typography>
+                      ) : (
+                        <Image
+                          width={30}
+                          height={30}
+                          alt="icon"
+                          src={item.icon}
+                        />
+                      )}
+                    </ListItemButtonStyled>
+                  )}
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+        </StackStyled>
+        {settingsItem && (
+          <Box>
+            <ListItem disablePadding sx={{ paddingBottom: '8px' }}>
+              <ListItemButtonStyled
+                selected={pathname.includes(settingsItem.path)}
+                disableRipple
+                onClick={() => router.push(settingsItem.path)}
+              >
+                <Box
                   sx={{
-                    fontSize: {
-                      xs: '10px',
-                      md: '12px',
-                    },
-                    color: '#666A72',
+                    display: 'flex',
                   }}
                 >
-                  {t(item.label)}
-                </Typography>
+                  <Image
+                    src={settingsItem.icon}
+                    width={30}
+                    height={30}
+                    alt="icon"
+                  />
+                  {isOpen && (
+                    <Typography
+                      sx={{
+                        fontSize: {
+                          xs: '10px',
+                          md: '12px',
+                        },
+                        color: '#666A72',
+                        alignItems: 'center',
+                        paddingBottom: '4px',
+                      }}
+                    >
+                      {t(settingsItem.label)}
+                    </Typography>
+                  )}
+                </Box>
               </ListItemButtonStyled>
             </ListItem>
-          ))}
-        </List>
-      </StackStyled>
-    </DrawerStyled>
+          </Box>
+        )}
+      </DrawerStyled>
+      {/* Close button */}
+      <Box
+        sx={{
+          position: 'absolute',
+          zIndex: 1300,
+          border: '1px solid #DADADA',
+          borderRadius: '50%',
+          background: 'white',
+          right: '-15px', // Adjusting position based on the drawer state
+          top: '15px',
+          transition: 'right 0.3s ease',
+          cursor: 'pointer',
+          width: '30px',
+          height: '30px',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        <Image
+          src={isOpen ? '/icons/close-icon.svg' : '/icons/open-icon.svg'}
+          width={20}
+          height={20}
+          alt="arrow"
+        />
+      </Box>
+    </Box>
   );
 }
 
 const DrawerStyled = styled(Drawer)(({ theme, open }) => ({
   width: open ? DRAWER_WIDTH : DRAWER_WIDTH_COLLAPSED,
-  // flexShrink: 0,
+  flexShrink: 0,
   position: 'relative',
   '& .MuiDrawer-paper': {
     width: open ? DRAWER_WIDTH : DRAWER_WIDTH_COLLAPSED,
     boxSizing: 'border-box',
     overflowX: 'hidden',
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
+    transition: 'width 0.3s ease',
+    '::-webkit-scrollbar': {
+      width: '1px',
+    },
   },
 }));
 
 const StackStyled = styled(Stack)(({ theme }) => ({
   p: 1,
   justifyContent: 'space-between',
+  flexGrow: 1,
 }));
 
 const ListItemButtonStyled = styled(ListItemButton)(({ theme }) => ({
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  justifyContent: 'center',
+  paddingTop: '0',
+  paddingBottom: '0',
+  width: '250px',
   '& .MuiTypography-root': {
-    fontSize: {
-      xs: '10px',
-      md: '12px',
-    },
-    color: '#000000',
     height: '40px',
+    justifyContent: 'flex-start',
     alignItems: 'center',
     display: 'flex',
   },
   '&.Mui-selected': {
-    alignItems: 'center',
+    alignItems: 'flex-start',
     color: 'transparent',
     position: 'relative',
-    display: 'flex',
     transition: 'background 0.3s ease',
     background: theme.palette.lily.white.linear,
     '& .MuiTypography-root': {
@@ -133,5 +304,8 @@ const ListItemButtonStyled = styled(ListItemButton)(({ theme }) => ({
       background: theme.palette.bright.blue.linear,
       right: 0,
     },
+  },
+  '&:hover': {
+    background: 'none',
   },
 }));
