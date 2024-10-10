9633f75041e48d3c8be2483de5cc0aab527f5aaa
minhnhat-vietqr <minh.pn@vietqr.vn>
2024-10-10 12:13:34 +0700

    Refactor searchAPI to include searchTransaction function

--- src/api/search/searchService.js
+++ src/api/search/searchService.js
@@ -1,1 +1,16 @@
-export const searchAPI = {};
+import axiosInstance from '~/utils/getAxios';
+
+const searchTransaction = async (searchQuery, userID) => {
+  const currentDate = new Date();
+  const daysAgo = currentDate.getDate() - 30; // 30 days ago
+  const query = {
+    value: searchQuery,
+    userId: userID,
+    fromDate: daysAgo,
+    toDate: currentDate,
+  };
+  const res = await axiosInstance.get(`apitransactions/list/web-v2/${query}`);
+  return res;
+};
+
+export const searchAPI = { searchTransaction };
