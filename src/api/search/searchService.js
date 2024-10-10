efed6bdd53a6db663c944ad6c8686c46644b855c
minhnhat-vietqr <minh.pn@vietqr.vn>
2024-10-10 16:42:43 +0700

    Refactor searchService to use numOfPastDays instead of daysAgo in searchTransaction function

--- src/api/search/searchService.js
+++ src/api/search/searchService.js
@@ -2,11 +2,11 @@ import axiosInstance from '~/utils/getAxios';
 
 const searchTransaction = async (searchQuery, userID) => {
   const currentDate = new Date();
-  const daysAgo = currentDate.getDate() - 30; // 30 days ago
+  const numOfPastDays = currentDate.getDate() - 30; // 30 days ago
   const res = await axiosInstance.get('apitransactions/list/web-v2', {
     value: searchQuery,
     userId: userID,
-    fromDate: daysAgo,
+    fromDate: numOfPastDays,
     toDate: currentDate,
   });
   return res;
