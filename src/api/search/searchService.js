1ba067e8aa8b6fd925f104921f43affaccf1e72d
minhnhat-vietqr <minh.pn@vietqr.vn>
2024-10-14 19:50:36 +0700

    Refactor searchTransaction function to use camel case for userId and update date formatting

--- src/api/search/searchService.js
+++ src/api/search/searchService.js
@@ -2,17 +2,16 @@
 import dayjs from 'dayjs';
 import axiosInstance from '~/utils/getAxios';
 
-const searchTransaction = async ({ searchQuery, userID }) => {
-  const currentDate = dayjs().format('YYYY-MM-DD HH:mm:ss');
-  const numOfPastDays = dayjs()
-    .subtract(30, 'day')
-    .format('YYYY-MM-DD HH:mm:ss');
+export const DATE_REGEX = 'YYYY-MM-DD HH:mm:ss';
 
+const searchTransaction = async ({ searchQuery, userId }) => {
+  const currentDate = dayjs();
+  const numOfPastDays = currentDate.subtract(30, 'day');
   const params = {
     value: searchQuery,
-    userId: userID,
-    fromDate: numOfPastDays,
-    toDate: currentDate,
+    userId: userId,
+    fromDate: numOfPastDays.format(DATE_REGEX),
+    toDate: currentDate.format(DATE_REGEX),
     offset: 1,
   };
   const res = await axiosInstance.get('transactions/list/web-v2', {
